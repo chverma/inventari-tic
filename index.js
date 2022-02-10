4c89918bfa0b9f14e7b9ffce52e6ee225d016fb3
chverma <me@chverma.com>
2022-02-10 09:21:22 +0100

    Remove google auth

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use((req,res,next) => {
     console.log("-----------------log route", req.path)
     next();
 })
-app.use(checkUser);
+
 
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,26 +32,12 @@ app.use(bodyParser.text({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 
-function checkUser(req, res, next) {
-  console.log("checkUser", req.path)
-  if ((req.path === '/login' || req.path === '/oauthCallback') || (req.session && req.session.tokens!==undefined)) {
-    return next();
-  } else {
-    //authenticate user
-    return res.redirect(301, '/login');
-  }
-
-
-}
-
 var routes = require('./app/routes/routes'); // importing route
 routes(app); // register the routes
 var incidenceRoutes = require('./app/routes/routesIncidence'); // importing route
 incidenceRoutes(app);
 var faultRoutes = require('./app/routes/routesFaults'); // importing route
 faultRoutes(app);
-var signInRoutes = require('./app/routes/routesGoogle');
-signInRoutes(app);
 var userRoutes = require('./app/routes/routesUser');
 userRoutes(app);
 var proposalsRoutes = require('./app/routes/routesProposals');
