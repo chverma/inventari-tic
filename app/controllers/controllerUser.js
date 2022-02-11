3498aa22130a0063139c1db1479d38411fffa46f
chverma <chverma@users.noreply.github.com>
2022-02-11 16:20:57 +0100

    Do some changes on user model

--- app/controllers/controllerUser.js
+++ app/controllers/controllerUser.js
@@ -1,5 +1,16 @@
 'use strict';
 
-exports.getUserData = function (req, res) {
-  res.send(req.session.userData);
+exports.getUserData = function(req, res) {
+    res.send(req.session.userData);
 };
+// Code to encrypt password
+/*bcrypt.hash(password, 10, function(err, hash) {
+  var sql = "INSERT INTO curd_table (first_name,last_name,username,password) VALUES ?";
+  var values = [
+      [firstname, lastname, username, hash]
+  ]
+  con.query(sql, [values], function(err, result, fields) {
+      if (err) throw err;
+      res.send({ message: 'Table Data', Total_record: result.length, result: result });
+  });
+});*/
--- app/model/modelUser.js
+++ app/model/modelUser.js
@@ -2,22 +2,19 @@
 var sql = require('./db.js');
 
 // Incidence object constructor
-var User = function (user) {
-  this.email = user.email;
-  this.userName = user.userName;
-  this.firstFamilyName = user.firstFamilyName;
-  this.secondFamilyName = user.secondFamilyName;
+var User = function(user) {
+    this.email = user.email;
+    this.password = user.password;
 };
 
-User.getUserData = function (result) {
-  sql.query('Select * from faltes', function (err, res) {
-    if (err) {
-      console.error('error: ', err);
-      result(null, err);
-    } else {
-      result(null, res);
-    }
-  });
+User.getUserData = function(result) {
+    sql.query('Select * from administrator', function(err, res) {
+        if (err) {
+            console.error('error: ', err);
+            result(null, err);
+        } else {
+            result(null, res);
+        }
+    });
 };
-
 module.exports = User;
