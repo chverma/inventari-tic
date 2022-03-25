791906a26dcc1f8b4d1ae1e64557792e8aac018a
chverma <chverma@users.noreply.github.com>
2022-03-25 15:53:06 +0100

    Add trigger in order to autocomplet some fields on inventary from SAI

--- app/model/db_inventari_tic.sql
+++ app/model/db_inventari_tic.sql
@@ -15,36 +15,32 @@ CREATE DATABASE IF NOT EXISTS `db_inventari_tic` DEFAULT CHARACTER SET utf8 COLL
 USE `db_inventari_tic`;
 
 CREATE TABLE IF NOT EXISTS `administrators` (
-  `admin_id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `email` varchar(300) NOT NULL,
-  `password` varchar(100) NOT NULL
+  `admin_id` INT(11) AUTO_INCREMENT PRIMARY KEY,
+  `email` VARCHAR(300) NOT NULL,
+  `password` VARCHAR(100) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
 
 CREATE TABLE IF NOT EXISTS `location` (
-  `location_id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `aula` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
-  `observacions` varchar(300) COLLATE utf8_spanish_ci NOT NULL
+  `location_id` INT(11) AUTO_INCREMENT PRIMARY KEY,
+  `aula` VARCHAR(60) COLLATE utf8_spanish_ci NOT NULL,
+  `observacions` VARCHAR(300) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
 
 CREATE TABLE IF NOT EXISTS `inventory` (
-  `inventory_id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `num_serie` varchar(150) NOT NULL,
-  `text_etiqueta` varchar(150) DEFAULT NULL,
-  `descripcio` varchar(400) DEFAULT NULL,
-  `observacions` varchar(400) DEFAULT NULL,
-  `cod_article` varchar(150) DEFAULT NULL,
-  `marca` varchar(150) DEFAULT NULL,
-  `model` varchar(150) DEFAULT NULL,
-  `id_inventari_sai` varchar(150) DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `type_id` int(11) NOT NULL,
-  `location_id` int(11) NOT NULL,
+  `inventory_id` INT(11) AUTO_INCREMENT PRIMARY KEY,
+  `num_serie` VARCHAR(150) NOT NULL,
+  `text_etiqueta` VARCHAR(150) DEFAULT NULL,
+  `descripcio` VARCHAR(400) DEFAULT NULL,
+  `observacions` VARCHAR(400) DEFAULT NULL,
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `type_id` INT(11) NOT NULL,
+  `location_id` INT(11) NOT NULL,
   FOREIGN KEY(`location_id`) REFERENCES `location`(`location_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
 
 CREATE TABLE IF NOT EXISTS `types` (
-  `type_id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `descripcio` varchar(25) NOT NULL
+  `type_id` INT(11) AUTO_INCREMENT PRIMARY KEY,
+  `descripcio` VARCHAR(25) NOT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
 
 INSERT INTO
@@ -58,15 +54,46 @@ VALUES
   (5, 'switch');
 
 CREATE TABLE IF NOT EXISTS `inventory_sai` (
-  `inventory_sai_id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `sai_id` varchar(150) NOT NULL,
-  `estat` varchar(150) NOT NULL,
-  `tipus` varchar(150) NOT NULL,
-  `cod_article` varchar(150) DEFAULT NULL,
-  `desc_cod_article` varchar(150) DEFAULT NULL,
-  `num_serie` varchar(150) NOT NULL,
-  `fabricant` varchar(150) DEFAULT NULL,
-  `model` varchar(150) DEFAULT NULL,
-  `espai_desti` varchar(150) DEFAULT NULL,
-  `desc_espai_desti` varchar(150) DEFAULT NULL
+  `inventory_sai_id` INT(11) AUTO_INCREMENT PRIMARY KEY,
+  `sai_id` VARCHAR(150) NOT NULL,
+  `estat` VARCHAR(150) NOT NULL,
+  `tipus` VARCHAR(150) NOT NULL,
+  `cod_article` VARCHAR(150) DEFAULT NULL,
+  `desc_cod_article` VARCHAR(150) DEFAULT NULL,
+  `num_serie` VARCHAR(150) NOT NULL,
+  `fabricant` VARCHAR(150) DEFAULT NULL,
+  `model` VARCHAR(150) DEFAULT NULL,
+  `espai_desti` VARCHAR(150) DEFAULT NULL,
+  `desc_espai_desti` VARCHAR(150) DEFAULT NULL
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8 COLLATE = utf8_spanish_ci;
+
+DELIMITER $$
+
+CREATE TRIGGER before_inventory_insert
+BEFORE INSERT
+ON inventory FOR EACH ROW
+BEGIN
+  DECLARE tipus_var VARCHAR(150);
+  DECLARE des_article_var VARCHAR(150);
+  DECLARE type_id_var INT(11);
+
+  SELECT tipus, desc_cod_article 
+  INTO
+    tipus_var, des_article_var
+  FROM inventory_sai
+  WHERE num_serie LIKE CONCAT("%", NEW.num_serie);
+
+  IF des_article_var IS NOT NULL THEN
+    SET NEW.descripcio = des_article_var;
+
+    SELECT type_id
+    INTO
+      type_id_var
+    FROM types
+    WHERE descripcio LIKE tipus_var;
+
+    SET NEW.type_id = type_id_var;
+  END IF;
+END$$
+
+DELIMITER ;
