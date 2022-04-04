1339244861e88f313222cc27fc05368ac9c637f1
chverma <chverma@users.noreply.github.com>
2022-04-04 16:45:12 +0200

    Allow to generate more than one labels pages

--- app/controllers/generateLabels.js
+++ app/controllers/generateLabels.js
@@ -24,87 +24,89 @@ const logoFile = path.join(controllerLabelsDataDir, 'logo_iestacio.png');
 const logoImgData = "data:image/png;base64," + fs.readFileSync(logoFile, 'base64');
 
 exports.generateLabels = function(inventory_items, req, res) {
-    const schemasObj = {};
-    const inputs = [];
-    const inputsObj = {};
     const colHorizontalIncrement = 65;
     const colVerticalIncrement = 30;
     const itemLength = inventory_items.length;
+    const itemsPerPage = 27;
     let itemCount = 0;
+    let pagesLength = Math.ceil(itemLength / itemsPerPage);
+    let inputs = [];
+    let schemasObj = {};
+    for (let page = 0; page < pagesLength; page++) {
+        let inputsObj = {};
+        let row = 0;
+        let col = 0;
+        while (itemCount < itemLength && row < 9) {
+            col = 0;
+            while (itemCount < itemLength && col < 3) {
+                let newObjKey = '';
+                // QR
+                newObjKey = "qrcode" + row + "-" + col;
+                let position = {
+                    "x": 12 + col * colHorizontalIncrement,
+                    "y": 12 + row * colVerticalIncrement
+                };
+                schemasObj[newObjKey] = new schemaObj('qrcode', position, 20, 20, 0);
+                inputsObj[newObjKey] = process.env.INVENTORY_HOST + '/detallInventari.html?inventory_id=' + inventory_items[itemCount].inventory_id;
 
-    let row = 0;
-    let col = 0;
-    while (itemCount < itemLength && row < 9) {
-        col = 0;
-        while (itemCount < itemLength && col < 3) {
-            let newObjKey = '';
-            // QR
-            newObjKey = "qrcode" + row + "-" + col;
-            let position = {
-                "x": 12 + col * colHorizontalIncrement,
-                "y": 12 + row * colVerticalIncrement
-            };
-            schemasObj[newObjKey] = new schemaObj('qrcode', position, 20, 20, 0);
-            inputsObj[newObjKey] = process.env.INVENTORY_HOST + '/detallInventari.html?inventory_id=' + inventory_items[itemCount].inventory_id;
+                // Text type
+                newObjKey = "type" + row + "-" + col;
+                position = {
+                    "x": 36 + col * colHorizontalIncrement,
+                    "y": 12 + row * colVerticalIncrement
+                };
+                schemasObj[newObjKey] = new schemaObj('text', position, 60, 7, 12);
+                inputsObj[newObjKey] = inventory_items[itemCount].text_etiqueta;
 
-            // Text type
-            newObjKey = "type" + row + "-" + col;
-            position = {
-                "x": 36 + col * colHorizontalIncrement,
-                "y": 12 + row * colVerticalIncrement
-            };
-            schemasObj[newObjKey] = new schemaObj('text', position, 60, 7, 12);
-            inputsObj[newObjKey] = inventory_items[itemCount].text_etiqueta;
+                // Text location
+                newObjKey = "location" + row + "-" + col;
+                position = {
+                    "x": 36 + col * colHorizontalIncrement,
+                    "y": 21 + row * colVerticalIncrement
+                };
+                schemasObj[newObjKey] = new schemaObj('text', position, 60, 7, 12);
+                inputsObj[newObjKey] = inventory_items[itemCount].aula;
 
-            // Text location
-            newObjKey = "location" + row + "-" + col;
-            position = {
-                "x": 36 + col * colHorizontalIncrement,
-                "y": 21 + row * colVerticalIncrement
-            };
-            schemasObj[newObjKey] = new schemaObj('text', position, 60, 7, 12);
-            inputsObj[newObjKey] = inventory_items[itemCount].aula;
+                // Text NS
+                newObjKey = "ns" + row + "-" + col;
+                position = {
+                    "x": 36 + col * colHorizontalIncrement,
+                    "y": 27 + row * colVerticalIncrement
+                };
+                schemasObj[newObjKey] = new schemaObj('text', position, 60, 7, 12);
+                inputsObj[newObjKey] = inventory_items[itemCount].num_serie;
 
-            // Text NS
-            newObjKey = "ns" + row + "-" + col;
-            position = {
-                "x": 36 + col * colHorizontalIncrement,
-                "y": 27 + row * colVerticalIncrement
-            };
-            schemasObj[newObjKey] = new schemaObj('text', position, 60, 7, 12);
-            inputsObj[newObjKey] = inventory_items[itemCount].num_serie;
+                // Logo image
+                newObjKey = "logo" + row + "-" + col;
+                position = {
+                    "x": 55 + col * colHorizontalIncrement,
+                    "y": 8.5 + row * colVerticalIncrement
+                };
+                schemasObj[newObjKey] = new schemaObj('image', position, 15, 15, 0);
+                inputsObj[newObjKey] = logoImgData;
 
-            // Logo image
-            newObjKey = "logo" + row + "-" + col;
-            position = {
-                "x": 55 + col * colHorizontalIncrement,
-                "y": 8.5 + row * colVerticalIncrement
-            };
-            schemasObj[newObjKey] = new schemaObj('image', position, 15, 15, 0);
-            inputsObj[newObjKey] = logoImgData;
-
-            col++;
-            itemCount++;
+                col++;
+                itemCount++;
+            }
+            row++;
         }
-        row++;
+        inputs.push(inputsObj);
     }
 
-    inputs.push(inputsObj);
-
-    const template = {
+    let template = {
         basePdf: BLANK_PDF,
         schemas: [schemasObj],
     };
 
     //console.log(JSON.stringify(template), JSON.stringify(inputs))
-
     generate({ template, inputs }).then((pdf) => {
         var readStream = new stream.PassThrough();
         readStream.end(pdf);
-
         res.set('Content-disposition', 'attachment; filename=etiquetes.pdf');
         res.set('Content-Type', 'application/pdf');
 
         readStream.pipe(res);
+        return pdf;
     });
+
 }
